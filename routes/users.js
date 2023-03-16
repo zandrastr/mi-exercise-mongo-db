2b98ea86259ec6edaf119b160834b24a5e81d772
Sandra S <zandrastr@hotmail.com>
2023-03-16 19:48:47 +0100

    create add user post route and print results

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,30 @@ router.get('/', function(req, res, next) {
   //When previous step is successful, .then (move on to...)
   .then(results => {
     console.log("Results: ", results);
+
+    let printUsers = "<h1>All users:</h1> <ul>"
+
+    //Loop through the results
+    for (user in results) {
+      printUsers += "<li>" + results[user].userName + "</li>"
+    }
+
+    printUsers += "</ul>"
+
+    res.send(printUsers);
+  })
+});
+
+//POST to database
+router.post("/add", function(req, res) {
+
+  req.app.locals.db.collection("users").insertOne(req.body)
+
+  .then(result => {
+    console.log(result);
+    res.redirect("/show");
   })
 
-  res.send('respond with a resource');
-});
+})
 
 module.exports = router;
